c44940121fc24ba396cfbe58fac60ed46b9b0897
Enso <aarmand.inbox@gmail.com>
2019-01-04 11:48:43 +0100

    updated, no Rehydrate

--- App.js
+++ App.js
@@ -1,53 +1,57 @@
 import React from "react";
 import { View, Text } from "react-native";
-import ApolloClient, { createNetworkInterface } from "apollo-client";
+// import ApolloClient, { createNetworkInterface } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { Font } from "expo";
-import { compose, createStore, combineReducers, applyMiddleware } from "redux";
-import { offline } from "redux-offline";
-import config from "redux-offline/lib/defaults";
+// import { compose, createStore, combineReducers, applyMiddleware } from "redux";
+// import { offline } from "redux-offline";
+// import config from "redux-offline/lib/defaults";
+import ApolloClient from "apollo-boost";
 
-import Rehydrate from "./Rehydrate";
+// import Rehydrate from "./Rehydrate";
 import { component as Root } from "./lib/js/re/root.bs.js";
 
 console.ignoredYellowBox = [
   "Warning: View.propTypes has been deprecated and will be removed in a future version of ReactNative. Use ViewPropTypes instead."
 ];
 
-const networkInterface = createNetworkInterface({
-  uri: `https://api.graph.cool/simple/v1/cj5cm0t4pnljk01087tv4g61a`
-});
-
 const client = new ApolloClient({
-  networkInterface
+  uri: "https://api.graph.cool/simple/v1/cj5cm0t4pnljk01087tv4g61a"
 });
+// const networkInterface = createNetworkInterface({
+//   uri: `https://api.graph.cool/simple/v1/cj5cm0t4pnljk01087tv4g61a`
+// });
 
-const store = createStore(
-  combineReducers({
-    apollo: client.reducer(),
-    rehydrate: (state = false, action) => {
-      switch (action.type) {
-        case "REHYDRATE_STORE":
-          return true;
-        default:
-          return state;
-      }
-    }
-  }),
-  undefined,
-  compose(
-    applyMiddleware(client.middleware()),
-    offline({
-      ...config,
-      persistCallback: () => {
-        store.dispatch({ type: "REHYDRATE_STORE" });
-      },
-      persistOptions: {
-        blacklist: ["rehydrate"]
-      }
-    })
-  )
-);
+// const client = new ApolloClient({
+//   networkInterface
+// });
+
+// const store = createStore(
+//   combineReducers({
+//     apollo: client.reducer(),
+//     rehydrate: (state = false, action) => {
+//       switch (action.type) {
+//         case "REHYDRATE_STORE":
+//           return true;
+//         default:
+//           return state;
+//       }
+//     }
+//   }),
+//   undefined,
+//   compose(
+//     applyMiddleware(client.middleware()),
+//     offline({
+//       ...config,
+//       persistCallback: () => {
+//         store.dispatch({ type: "REHYDRATE_STORE" });
+//       },
+//       persistOptions: {
+//         blacklist: ["rehydrate"]
+//       }
+//     })
+//   )
+// );
 
 export default class App extends React.Component {
   state = {
@@ -70,10 +74,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: "rgb(54, 97, 115)" }}>
-        <ApolloProvider client={client} store={store}>
-          <Rehydrate>
-            {this.state.fontLoaded ? <Root /> : null}
-          </Rehydrate>
+        <ApolloProvider client={client}>
+          <Root />
         </ApolloProvider>
       </View>
     );
